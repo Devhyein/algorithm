312f0a422c58ca6fe8a75c1bd552b8b29727d6c3
Devhyein <jhn7121@naver.com>
2019-09-03 01:56:09 +0900

    not finished yet

---
+++ Algo_1018.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.util.*;
+
+public class Algo_1018 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int m = s.nextInt();
+		String[] str = new String[n];
+		
+		for(int i=0;i<n;i++) {
+			str[i] = s.next();
+		}
+	}
+}
